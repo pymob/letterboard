22a43b8f1798f06ceb93e863bd234c655e149591
Max Scheerschmidt <maxscheerschmidt@gmail.com>
2019-06-01 18:12:26 +0200

    Initial commit

---
+++ src/main/java/de/max/letterboard/LetterboardApplication.java
@@ -0,0 +1,13 @@
+package de.max.letterboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetterboardApplication.class, args);
+    }
+
+}
